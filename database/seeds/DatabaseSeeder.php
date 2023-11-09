934e827b7a25e27da3fd6738f834ec8d6a47b65e
sytnic <sytnic@list.ru>
2023-11-09 10:28:56 +0300

    6. Database is ready

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,12 +5,16 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // числа указывают количество будущих строк
+        factory(App\User::class, 5)->create();
+        factory(App\Poll::class, 10)->create();
+        factory(App\Question::class, 50)->create();
+        factory(App\Answer::class, 500)->create();
     }
 }
